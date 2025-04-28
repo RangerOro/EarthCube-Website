b3bc2c5265dee1a1bbf5467ff46a131fff63e7af
Ranger_Oro <nikmariotti11@gmail.com>
2025-04-28 22:17:10 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/EarthCube logo-favicon.png',
 
   // Set the production url of your site here
-  url: 'https://https://testing-website-earthcube.vercel.app',
+  url: 'https://https://test-website-earthcube.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
